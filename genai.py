db9cd27afbc349b77856b6fc4a0af0c8e9b8e483
muneeb1505 <150434834+muneeb1505@users.noreply.github.com>
2024-06-28 17:59:06 +0530

    Add files via upload

---
+++ genai.py
@@ -0,0 +1,51 @@
+import google.generativeai as genai
+from flask import Flask, render_template, request
+from dotenv import load_dotenv
+import os
+
+app = Flask(__name__)
+
+load_dotenv()
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+model = genai.GenerativeModel('gemini-pro')
+chat = model.start_chat(history=[])
+
+responses = []
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        question = request.form.get('question')
+        if not question.strip():
+            response = "Please give Prompt !!"
+        else:
+            response = chat.send_message(question).text
+        responses.insert(0, {'response': response})
+    return render_template('genai.html',responses=responses)
+
+@app.route('/feedback')
+def feedback():
+    return render_template('feedback.html')
+
+@app.route('/about')
+def about():
+    return render_template('genAbout.html')
+
+@app.route('/services')
+def services():
+    return render_template('genServices.html')
+
+@app.route('/contactus')
+def contact():
+    return render_template('YTContact.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
